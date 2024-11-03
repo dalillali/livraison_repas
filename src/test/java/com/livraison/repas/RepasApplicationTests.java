abb1352cd0b1bcc34f636f4657286b3dc6c62fe8
ali-dalil <alidaliletude@gmail.com>
2024-11-03 23:17:42 +0100

    Initial commit of Spring Boot application with Docker setup

---
+++ src/test/java/com/livraison/repas/RepasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livraison.repas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
